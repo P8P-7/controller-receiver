ce991d8e9be80697e7319b79e69637d9df8a75eb
Travis CI <travis@travis-ci.org>
2018-06-13 13:57:16 +0000

    Deploy Goliath docs to GitHub Pages. Build: 30
    Commit: 51b827175e3c06be4a0f7f273bf83c9c56825bf4

--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['send',['send',['../classgoliath_1_1btc_1_1BluetoothController.html#ac58ce8c6f421b3194c31ea097d385dd2',1,'goliath::btc::BluetoothController']]],
   ['setconfig',['setConfig',['../map_8h.html#aab9f8e3b828fa42072d47012fdee41ea',1,'map.cpp']]],
   ['status',['Status',['../namespacegoliath_1_1btc.html#a899b26c5ac9b2abbb01cb9f639490788',1,'goliath::btc']]],
   ['statusmessage',['StatusMessage',['../structgoliath_1_1btc_1_1StatusMessage.html',1,'goliath::btc']]]
--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['send',['send',['../classgoliath_1_1btc_1_1BluetoothController.html#ac58ce8c6f421b3194c31ea097d385dd2',1,'goliath::btc::BluetoothController']]],
   ['setconfig',['setConfig',['../map_8h.html#aab9f8e3b828fa42072d47012fdee41ea',1,'map.cpp']]]
 ];
