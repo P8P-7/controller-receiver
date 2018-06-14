014282cdcd979b9ade95a77618d1b94b768f1fa3
Travis CI <travis@travis-ci.org>
2018-06-14 21:11:12 +0000

    Deploy Goliath docs to GitHub Pages. Build: 34
    Commit: b6ee2347ec02bad1773c3d78de3797f7fb5ec929

--- search/functions_7.js
+++ search/functions_7.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['send',['send',['../classgoliath_1_1btc_1_1BluetoothController.html#a8a2a5f781e50abf3f0058623de60c873',1,'goliath::btc::BluetoothController']]],
   ['sendlast',['sendLast',['../classgoliath_1_1btc_1_1BluetoothController.html#a5bedb149e8a79355a306984143cd53fe',1,'goliath::btc::BluetoothController']]],
+  ['sendtocontroller',['sendToController',['../main_8cpp.html#aea5b85a9435f6ba46877de6169defd12',1,'main.cpp']]],
   ['setconfig',['setConfig',['../map_8h.html#aab9f8e3b828fa42072d47012fdee41ea',1,'map.cpp']]],
   ['start',['start',['../classgoliath_1_1btc_1_1BluetoothController.html#a19b62e2682bbd6bb0854376dba9f3e27',1,'goliath::btc::BluetoothController']]],
   ['stringtocommandcase',['stringToCommandCase',['../convert_8h.html#adf49b826563e6728a3a2249462723e85',1,'convert.cpp']]],
