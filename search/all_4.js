159db1d863d33e166c88e64087827086f8c419cd
Travis CI <travis@travis-ci.org>
2018-06-20 09:33:19 +0000

    Deploy Goliath docs to GitHub Pages. Build: 37
    Commit: cbc712dc7d9de3336404094a824f1ea6d4cecedf

--- search/all_4.js
+++ search/all_4.js
@@ -3,6 +3,5 @@ var searchData=
   ['initconfig',['initConfig',['../map_8h.html#a5af1011f0f921aef06ecdfc27d7bf791',1,'map.cpp']]],
   ['initcontrols',['initControls',['../main_8cpp.html#ade2ac8db32081bbf24ad4ec0b505910e',1,'main.cpp']]],
   ['input',['Input',['../structgoliath_1_1btc_1_1Input.html',1,'goliath::btc']]],
-  ['inputerror',['InputError',['../namespacegoliath_1_1btc.html#ab4f87cdc9af755884bc25d7154cfdfb7',1,'goliath::btc']]],
-  ['inputtocommand',['inputToCommand',['../convert_8h.html#ae1f23f95b5332cb9c4d2d5db2371365d',1,'convert.h']]]
+  ['inputerror',['InputError',['../namespacegoliath_1_1btc.html#ab4f87cdc9af755884bc25d7154cfdfb7',1,'goliath::btc']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['initconfig',['initConfig',['../map_8h.html#a5af1011f0f921aef06ecdfc27d7bf791',1,'map.cpp']]],
-  ['initcontrols',['initControls',['../main_8cpp.html#ade2ac8db32081bbf24ad4ec0b505910e',1,'main.cpp']]],
-  ['inputtocommand',['inputToCommand',['../convert_8h.html#ae1f23f95b5332cb9c4d2d5db2371365d',1,'convert.h']]]
+  ['initcontrols',['initControls',['../main_8cpp.html#ade2ac8db32081bbf24ad4ec0b505910e',1,'main.cpp']]]
 ];
